cbea4ed1fe8b4acffc51238e17d2760b49abc8cd
Leo-Lucsva <117685619+Leo-Lucsva@users.noreply.github.com>
2022-12-12 17:49:48 -0300

    Add files via upload

--- Trabalho.py
+++ Trabalho.py
@@ -1,58 +1,38 @@
-print('-=' * 20)
-print(f'{"BEM VINDO AO IFOOD DA ESQUINA":^40}')
-print('-=' * 20)
-
 # Criar um programa de i food, onde a a pessoa vai logar com senha e nome;
 # vai escolher qual lanche vai querer e esse lanche será adicionado a um carrinho 
 # adicionar opção para ver o carrinho
 # quando ela selecionar tudo o que vai querer, o endereço caso for entrega (R$ 5 entrega) ou retirada
 # e o valor total da compra, além da forma de pagamento
 
-def fazer_login ():
-    pass
+# O QUE O CLIENTE QUER FAZER
 
-def criar_login ():
-    a = open('dados_conta', 'r').read()
-    print(a)
-    nome_conta = str(input('Digite seu apelido (Lembre-se dele): '))
-    senha_conta = str(input('Digite sua senha: '))
-
-
-print('Antes de fazer o seu pedido, faça login...')
-
-opcao_logar = int(input('''Digite:
+opcao_login = int(input('''Digite:
 [1] Para criar conta
-[2] Para acessar conta
+[2] Para fazer login
 '''))
 
-if opcao_logar == 1:
-    criar_login()
+match opcao_login:
+    case 1:
+        criar_conta()
+    case 2:
+        fazer_login
 
-'''
-if opcao_logar == 1:
-    conta = str(input('Digite qual será seu apelido (Lembre-se dele): '))
-    senha = str(input('Digite qual será sua senha: '))
+#FAZER LOGIN
 
-    dados_conta = open('dados_conta', 'w')
-    dados_senha = open('dados_senha', 'w')
-    
-    dados_conta.write(conta)
-    dados_senha.write(senha)
-
-    dados_conta.close()
-    dados_senha.close()
-
-    print('CONTA CRIADA COM SUCESSO!')
-
-elif opcao_logar == 2:
-    dados_conta = open('dados_conta', 'r').read()
-    dados_senha = open('dados_senha', 'r').read()
-    
-    conta = str(input('Digite seu apelido: '))
+def fazer_login ():
+    nome = str(input('Digite o seu apelido: '))
     senha = str(input('Digite sua senha: '))
+    
 
-    if (conta == dados_conta) and senha == dados_senha:
-        print('DADOS CORRETOS! VOCÊ ENTROU!', 'Olá', conta)
-    else:
-        print('DADOS INVÁLIDOS, TENTE NOVAMENTE!')
-        '''
+#CRIAR CONTA
+def criar_conta ():
+    dados_conta = open('dados_conta', 'w')
+
+    nome = str(input('Digite seu apelido (Lembre-se dele): '))
+    senha = str(input('Digite a senha: '))
+
+    dados_conta.write(f'{nome} {senha}')
+    dados_conta.close()
+
+    dados_conta = open('dados_conta', 'r').read()
+    print(dados_conta)
