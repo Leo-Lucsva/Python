8eeb4e4c1013a84941c442820ab6ac02f1b506ed
Leo-Lucsva <117685619+Leo-Lucsva@users.noreply.github.com>
2022-12-14 21:28:41 -0300

    Update Código de cadastro.py

--- Código de cadastro.py
+++ Código de cadastro.py
@@ -5,6 +5,19 @@ from os.path import exists
 from os import getcwd as getpath
 from time import sleep
 
+from os.path import exists
+from os import getcwd as getpath
+from time import sleep
+from random import randint
+
+print('-=' * 15)
+print(f"{'BEM VINDO AO LLS BURGUER!!':^30}")
+print('-=' * 15)
+sleep(1)
+print('Primeiro faça login para fazer seu pedido!')
+sleep(1)
+
+
 #VERIFICAR SE O CADASTRO EXISTE OU ESTÁ CERTO
 def verificar_login (nome, senha, cadastro):
     dados_conta = open('dados_conta', 'r').read().split()
@@ -14,12 +27,12 @@ def verificar_login (nome, senha, cadastro):
        
             if dado_nome == nome:  
                 if cadastro == True:
-                    print('falsoooo')
                     return False
                 if dado_senha == senha:
                     return True    
                 else: 
                     return False
+    return False
 
 #CRIAR CONTA
 def criar_conta ():
@@ -27,17 +40,24 @@ def criar_conta ():
 
     nome = str(input('Digite seu apelido sem espaços (Lembre-se dele): '))
     senha = str(input('Digite a senha: '))
-    if not verificar_login (nome, senha, True):
+    if verificar_login (nome, senha, True):
         dados_conta.write(f' {nome} {senha} ')
         dados_conta.close()
 
         sleep(1)
         dados_conta = open('dados_conta', 'r').read()
-        print(dados_conta)
         print('CONTA REGISTRADA! AGORA FAÇA LOGIN!')
     else:
-        print('APELIDO OU CONTA JÁ REGISTRADA!.... FAÇA LOGIN ABAIXO:')
-        
+        sleep(0.5)
+        print('-' * 15)
+        print('APELIDO OU CONTA JÁ REGISTRADA!.... O QUE DESEJA FAZER? ')
+        opcao = int(input('''
+[1] Tentar criar conta novamente
+[2] Fazer login
+        '''))
+
+        if opcao == 1:
+            criar_conta()
         sleep(0.7)
     fazer_login()
 
@@ -54,6 +74,7 @@ def fazer_login ():
         senha = str(input('Digite sua senha: '))
 
         if verificar_login(nome, senha, False):
+            sleep(0.7)
             print(f'CADASTRO EFETUADO COM SUCESSO! É bom te ver por aqui {nome}!')
         else:
             print('APELIDO OU SENHA INCORRETOS!')
@@ -72,9 +93,9 @@ def fazer_login ():
 
 # O QUE O CLIENTE QUER FAZER
 def inicio_login ():
-    opcao_login = int(input('''Digite:
-    [1] Para criar conta
-    [2] Para fazer login
+    opcao_login = int(input('''Digite o que deseja:
+[1] Para criar conta
+[2] Para fazer login
     '''))
 
     match opcao_login:
